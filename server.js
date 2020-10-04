ac7c3c2450bdb466e8a9cbf99e47d264b1b0b029
Nehal <nehal@Aashishs-MacBook-Air.local>
2020-10-04 19:31:15 -0400

    render html via fs read

--- server.js
+++ server.js
@@ -1,34 +1,6 @@
-// import https from 'https';
-// import http from 'http';
 import config from './config';
+import fs from 'fs';
 import express from 'express';
-
-// HTTPS
-/* 
-https.get('https://www.lynda.com', res => {
-  console.log('Response status code: ', res.statusCode);
-
-  res.on('data', chunk => {
-     console.log(chunk.toString());
-  });
-});
-
-// HTTP
-const server = http.createServer();
-
-server.listen(8080);
-
-server.on('request', (req, res) => {
-    res.write('Hello HTTP!\n');
-
-    setTimeout(() => {
-        res.write('I can stream!\n');
-        res.end();
-    }, 9000);
-});
-*/
-
-//
 
 const server = express();
 
@@ -39,7 +11,9 @@ server.get('/', (req, res) => {
 
  // routing
 server.get('/about.html', (req, res) => {
-    res.send('About Express!');
+    fs.readFile('./about.html', (err, data) => {
+        res.send(data.toString());
+    });
  });
 
 server.listen(config.port, () => {

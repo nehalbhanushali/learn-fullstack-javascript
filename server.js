40b0c9ca1ea858bd10dc37d7267bff1545d56b23
Nehal <nehal@Aashishs-MacBook-Air.local>
2020-10-04 19:39:03 -0400

    use static middleware to serve static assets like about.html

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ import express from 'express';
 const server = express();
 
 // routing
+// TODO: manage group of routes in their own module
+// e.g: All API requests to be managed in /api/index.js
 server.get('/', (req, res) => {
     res.send('Hello Express');
  });
 
- // routing
-server.get('/about.html', (req, res) => {
-    fs.readFile('./about.html', (err, data) => {
-        res.send(data.toString());
-    });
- });
+// use static middleware to automatically serve static assets like this about.html
+// public is where we want to host the static assets on the file system
+// NOTE: njinx or something alike would be the actual choice in a production environment
+// static middleware used to keep things simple
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
     console.info('Listening on port', config.port);

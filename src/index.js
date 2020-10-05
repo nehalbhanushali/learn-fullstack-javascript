d196d97f02f104ff097634a96bf35eb392cffc7b
Nehal <nehal@Aashishs-MacBook-Air.local>
2020-10-05 13:15:26 -0400

    component reusability via props

--- src/index.js
+++ src/index.js
@@ -4,20 +4,23 @@ import ReactDOM from 'react-dom';
 const color = Math.random() > 0.5 ? 'green' : 'red';
 
 // Component: Which is a function
-const App = () => {
+const App = (props) => {
     return (
-        // <h2 style={{color: color}}></h2>
-        // similar to html except certain things like className instead of class
-        // TODO: use components
         <h2 className='text-center' style={{color}}>
-            Hello React with JSX via component!
+            {props.headerMessage}
         </h2>
     );
 };
 
 ReactDOM.render(
-    // Top level component is usually App component
-    <App />,
+    // Top level component is usually App component as if it was a regular html
+    // Think of HTML components as built in component
+    // react extends our capabilities of HTML elements
+    // Also, reusable.
+    // lets say we have our custom prop headerMessage that can 
+    // be passed to the component above via the props parameter
+
+    <App headerMessage='Hello props!'/>, 
 
     // render using getElementById which is a DOM API
     document.getElementById('root')

ef532c633ddc0a325de8d8a65072012bbc1e375c
Nehal <nehal@Aashishs-MacBook-Air.local>
2020-10-05 13:09:39 -0400

    render via component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 
 const color = Math.random() > 0.5 ? 'green' : 'red';
 
+// Component: Which is a function
+const App = () => {
+    return (
+        // <h2 style={{color: color}}></h2>
+        // similar to html except certain things like className instead of class
+        // TODO: use components
+        <h2 className='text-center' style={{color}}>
+            Hello React with JSX via component!
+        </h2>
+    );
+};
+
 ReactDOM.render(
-    // <h2 style={{color: color}}></h2>
-    // similar to html except certain things like className instead of class
-    <h2 className='text-center' style={{color}}>
-        Hello React with JSX!
-    </h2>,
+    // Top level component is usually App component
+    <App />,
 
     // render using getElementById which is a DOM API
     document.getElementById('root')

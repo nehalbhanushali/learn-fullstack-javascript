5d9195c8c07b05180444a60dc59d7abf6e78eb94
Nehal <nehal@Aashishs-MacBook-Air.local>
2020-10-05 13:40:35 -0400

    PropTypes  validation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'; 
 
-const color = Math.random() > 0.5 ? 'green' : 'red';
-
-// Component: Which is a function
 const App = (props) => {
     return (
-        <h2 className='text-center' style={{color}}>
+        <h2 className='text-center'> 
             {props.headerMessage}
         </h2>
     );
 };
 
+// The props might need to be added to some kind of validation
+// did not get the linting error like in the video because it is 
+// probably covered in newer dependency versions
+App.propTypes = {
+    // React.PropTypes has moved into a different package since React v15.5. 
+    // Please use the prop-types library instead.
+    // We provide a codemod script to automate the conversion.
+    // headerMessage: React.PropTypes.string
+    headerMessage: PropTypes.string.isRequired,
+};
+
 ReactDOM.render(
-    // Top level component is usually App component as if it was a regular html
-    // Think of HTML components as built in component
-    // react extends our capabilities of HTML elements
-    // Also, reusable.
-    // lets say we have our custom prop headerMessage that can 
-    // be passed to the component above via the props parameter
-
+    // Theese should give warning in console
+    // <App headerMessage={4}/>, // for having wrong propType
+    // <App />, // for not having a proptype even though it is marked required
+    
     <App headerMessage='Hello props!'/>, 
-
-    // render using getElementById which is a DOM API
     document.getElementById('root')
 );

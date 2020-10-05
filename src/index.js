7c9abcde1e10e800a138bb9eeea7e5244c783ab1
Nehal <nehal@Aashishs-MacBook-Air.local>
2020-10-05 13:04:04 -0400

    variables using jsx, className, bootstrap

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const color = Math.random() > 0.5 ? 'green' : 'red';
+
 ReactDOM.render(
-    // the following will give us a simple Object with properties including type(h2) and
-    // props (-> children -> textContent)
-
-    // React.createElement('h2', null, 'Hello React with createElement'),
-    
-    // VIRTUAL DOM
-    // So in react we simply describe html as objects
-    // and then react put them together in a tree and keep a copy of this tree in memory.
-    // So when things change, react will re-calulate the tree and compares trees and takes 
-    // to the browser only the difference.
-
-    // JSX
-    // Now above way of create element is inconvinient as compared to witing HTML
-    <h3>Hello React with JSX! Thanks to Babel</h3>,
+    // <h2 style={{color: color}}></h2>
+    // similar to html except certain things like className instead of class
+    <h2 className='text-center' style={{color}}>
+        Hello React with JSX!
+    </h2>,
 
     // render using getElementById which is a DOM API
     document.getElementById('root')
-)
+);

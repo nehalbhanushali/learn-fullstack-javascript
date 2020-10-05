24a355d9c59180607966eefd5c4bc93df9284a9b
Nehal <nehal@Aashishs-MacBook-Air.local>
2020-10-04 23:08:40 -0400

    EJS

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ import apiRouter from './api';
 import express from 'express';
 const server = express();
 
+// express will look for .ejs templates  in the views folder
+server.set('view engine', 'ejs');
+
 // routing
 server.get('/', (req, res) => {
-    res.send('Hello Express');
+    // res.send('Hello Express');
+    // TODO: Instead of these strings
+    // use dynamic template language like EJS
+    // a new dependency to be brought in 
+    // res.render('index');
+    // or pass variables
+    res.render('index', {
+       content: 'Hello Express and <em>EJS</em>!'
+    });
+
  });
 
 // Now use it like express middleware

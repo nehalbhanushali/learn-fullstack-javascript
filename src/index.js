27f6543bc166a23bedc1a470785270e4119417d2
Nehal <nehal@Aashishs-MacBook-Air.local>
2020-10-05 13:44:51 -0400

    defaultProps

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 const App = (props) => {
     return (
+        // TODO: components composability
         <h2 className='text-center'> 
             {props.headerMessage}
         </h2>
@@ -14,18 +15,16 @@ const App = (props) => {
 // did not get the linting error like in the video because it is 
 // probably covered in newer dependency versions
 App.propTypes = {
-    // React.PropTypes has moved into a different package since React v15.5. 
-    // Please use the prop-types library instead.
-    // We provide a codemod script to automate the conversion.
-    // headerMessage: React.PropTypes.string
-    headerMessage: PropTypes.string.isRequired,
+    headerMessage: PropTypes.string,
+};
+
+App.defaultProps = {
+    headerMessage: "Hello!!",
 };
 
 ReactDOM.render(
-    // Theese should give warning in console
-    // <App headerMessage={4}/>, // for having wrong propType
-    // <App />, // for not having a proptype even though it is marked required
-    
-    <App headerMessage='Hello props!'/>, 
+    // This will use defaultProps
+    <App />,
+    // <App headerMessage='Hello props!'/>, 
     document.getElementById('root')
 );
